a08992344e6a4bcefe51956ef96911df4c3073c1
dagboujhatem <dagboujhatem@gmail.com>
2021-04-07 19:28:20 +0100

    refactoring connect JS file

--- database/connect.js
+++ database/connect.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/database001', {useNewUrlParser: true});
+mongoose.connect('mongodb://localhost:27017/database001', {useNewUrlParser: true, useUnifiedTopology: true})
+.then((success) => {
+    console.log("=> Successfully connection to database")
+}).catch((error) => {
+     console.log("=> Connect with error") 
+});
