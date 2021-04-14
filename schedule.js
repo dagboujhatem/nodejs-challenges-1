d5ace5ebebb123177a4abdd5c35ab5add210cf79
dagboujhatem <dagboujhatem@gmail.com>
2021-04-14 11:29:24 +0100

    stop cron

--- schedule.js
+++ schedule.js
@@ -1,7 +1,9 @@
 const cron = require('node-cron');
 
-cron.schedule('0 */2 * * * *', () => {
+const task = cron.schedule('0 */2 * * * *', () => {
   console.log('running a task every two minute.');
   //  Busness Logic ....
 
 });
+
+task.stop();
